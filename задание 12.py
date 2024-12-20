62be2d7dcb60ce5755fb29c7cca38bcbf9c0d789
accxille <ivanovazhanna378@yandex.ru>
2024-12-20 09:31:24 +0300

    задание 12.py

---
+++ задание 12.py
@@ -0,0 +1,8 @@
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        print(name)
+        print(salary)
+        print(salary + salary / 10)
+        
